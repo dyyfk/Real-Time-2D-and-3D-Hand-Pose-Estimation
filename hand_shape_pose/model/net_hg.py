67c1ad44987a1fea7c9481d26fdfc28a777bbfaa
dyyfk <36778268+dyyfk@users.noreply.github.com>
2021-12-15 15:57:40 -0800

    Update net_hg.py

--- hand_shape_pose/model/net_hg.py
+++ hand_shape_pose/model/net_hg.py
@@ -13,58 +13,58 @@ import torch.nn as nn
 from hand_shape_pose.util.net_util import Residual
 
 
-class Hourglass(nn.Module):
-    def __init__(self, n, nModules, nFeats):
-        super(Hourglass, self).__init__()
-        self.n = n
-        self.nModules = nModules
-        self.nFeats = nFeats
+# class Hourglass(nn.Module):
+#     def __init__(self, n, nModules, nFeats):
+#         super(Hourglass, self).__init__()
+#         self.n = n
+#         self.nModules = nModules
+#         self.nFeats = nFeats
 
-        _up1_, _low1_, _low2_, _low3_ = [], [], [], []
-        for j in range(self.nModules):
-            _up1_.append(Residual(self.nFeats, self.nFeats))
-        self.low1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        for j in range(self.nModules):
-            _low1_.append(Residual(self.nFeats, self.nFeats))
+#         _up1_, _low1_, _low2_, _low3_ = [], [], [], []
+#         for j in range(self.nModules):
+#             _up1_.append(Residual(self.nFeats, self.nFeats))
+#         self.low1 = nn.MaxPool2d(kernel_size=2, stride=2)
+#         for j in range(self.nModules):
+#             _low1_.append(Residual(self.nFeats, self.nFeats))
 
-        if self.n > 1:
-            self.low2 = Hourglass(n - 1, self.nModules, self.nFeats)
-        else:
-            for j in range(self.nModules):
-                _low2_.append(Residual(self.nFeats, self.nFeats))
-            self.low2_ = nn.ModuleList(_low2_)
+#         if self.n > 1:
+#             self.low2 = Hourglass(n - 1, self.nModules, self.nFeats)
+#         else:
+#             for j in range(self.nModules):
+#                 _low2_.append(Residual(self.nFeats, self.nFeats))
+#             self.low2_ = nn.ModuleList(_low2_)
 
-        for j in range(self.nModules):
-            _low3_.append(Residual(self.nFeats, self.nFeats))
+#         for j in range(self.nModules):
+#             _low3_.append(Residual(self.nFeats, self.nFeats))
 
-        self.up1_ = nn.ModuleList(_up1_)
-        self.low1_ = nn.ModuleList(_low1_)
-        self.low3_ = nn.ModuleList(_low3_)
+#         self.up1_ = nn.ModuleList(_up1_)
+#         self.low1_ = nn.ModuleList(_low1_)
+#         self.low3_ = nn.ModuleList(_low3_)
 
-        self.up2 = nn.Upsample(scale_factor=2)
+#         self.up2 = nn.Upsample(scale_factor=2)
 
-    def forward(self, x):
-        up1 = x
-        for j in range(self.nModules):
-            up1 = self.up1_[j](up1)
+#     def forward(self, x):
+#         up1 = x
+#         for j in range(self.nModules):
+#             up1 = self.up1_[j](up1)
 
-        low1 = self.low1(x)
-        for j in range(self.nModules):
-            low1 = self.low1_[j](low1)
+#         low1 = self.low1(x)
+#         for j in range(self.nModules):
+#             low1 = self.low1_[j](low1)
 
-        if self.n > 1:
-            low2 = self.low2(low1)
-        else:
-            low2 = low1
-            for j in range(self.nModules):
-                low2 = self.low2_[j](low2)
+#         if self.n > 1:
+#             low2 = self.low2(low1)
+#         else:
+#             low2 = low1
+#             for j in range(self.nModules):
+#                 low2 = self.low2_[j](low2)
 
-        low3 = low2
-        for j in range(self.nModules):
-            low3 = self.low3_[j](low3)
-        up2 = self.up2(low3)
+#         low3 = low2
+#         for j in range(self.nModules):
+#             low3 = self.low3_[j](low3)
+#         up2 = self.up2(low3)
 
-        return up1 + up2
+#         return up1 + up2
 
 
 class Net_HM_HG(nn.Module):
@@ -251,96 +251,95 @@ Used by 3-stage and 4-stage Identity Mapping Hourglass Network.
 #         return x
 
 
-# class Net_HM_HG(nn.Module):
-#     """Instantiate an n order Hourglass Network block using recursive trick."""
-# #     def __init__(self, num_joints, num_stages=2, num_modules=2, num_feats=256): # original Net_HM_HG
-#     def __init__(self, depth, nFeat=256, increase=128, bn=False, resBlock=Conv, num_joints=21, num_stages=2, num_modules=2, num_feats=256):
-#         super(Net_HM_HG, self).__init__()
-#         self.depth = depth  # oder number
-#         self.nFeat = nFeat  # input and output channels
-#         self.increase = increase  # increased channels while the depth grows
-#         self.bn = bn
-#         self.resBlock = resBlock
-#         # will execute when instantiate the Hourglass object, prepare network's parameters
-#         self.hg = self._make_hour_glass()
-#         self.downsample = nn.MaxPool2d(2, 2)  # no learning parameters, can be used any times repeatedly
-#         self.upsample = nn.Upsample(scale_factor=2, mode='nearest')  # no learning parameters  # FIXME: 改成反卷积？
+class Hourglass(nn.Module):
+    """Instantiate an n order Hourglass Network block using recursive trick."""
+    def __init__(self, depth, nFeat=256, increase=128, bn=False, resBlock=Conv):
+        super(Hourglass, self).__init__()
+        self.depth = depth  # oder number
+        self.nFeat = nFeat  # input and output channels
+        self.increase = increase  # increased channels while the depth grows
+        self.bn = bn
+        self.resBlock = resBlock
+        # will execute when instantiate the Hourglass object, prepare network's parameters
+        self.hg = self._make_hour_glass()
+        self.downsample = nn.MaxPool2d(2, 2)  # no learning parameters, can be used any times repeatedly
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')  # no learning parameters  # FIXME: 改成反卷积？
 
-#     def _make_single_residual(self, depth_id):
-#         # the innermost conve layer, return as a layer item
-#         return self.resBlock(self.nFeat + self.increase * (depth_id + 1), self.nFeat + self.increase * (depth_id + 1),
-#                              bn=self.bn)                            # ###########  Index: 6
+    def _make_single_residual(self, depth_id):
+        # the innermost conve layer, return as a layer item
+        return self.resBlock(self.nFeat + self.increase * (depth_id + 1), self.nFeat + self.increase * (depth_id + 1),
+                             bn=self.bn)                            # ###########  Index: 6
 
-#     def _make_lower_residual(self, depth_id):
-#         # return as a list
-#         pack_layers = [self.resBlock(self.nFeat + self.increase * depth_id, self.nFeat + self.increase * depth_id,
-#                                      bn=self.bn, relu=False),                                     # ######### Index: 0
-#                        self.resBlock(self.nFeat + self.increase * depth_id, self.nFeat + self.increase * (depth_id + 1),
-#                                                                                                   # ######### Index: 1
-#                                      bn=self.bn),
-#                        self.resBlock(self.nFeat + self.increase * (depth_id + 1), self.nFeat + self.increase * depth_id,
-#                                                                                                    # ######### Index: 2
-#                                      bn=self.bn),
-#                        self.resBlock(self.nFeat + self.increase * depth_id, self.nFeat + self.increase * depth_id,
-#                                                                                                    # ######### Index: 3
-#                                      bn=self.bn),
-#                        self.resBlock(self.nFeat + self.increase * depth_id, self.nFeat + self.increase * depth_id,
-#                                                                                                    # ######### Index: 4
-#                                      bn=self.bn, relu=False),
-#                        nn.LeakyReLU(negative_slope=0.01, inplace=True)                              # ######### Index: 5
-#                        ]
-#         return pack_layers
+    def _make_lower_residual(self, depth_id):
+        # return as a list
+        pack_layers = [self.resBlock(self.nFeat + self.increase * depth_id, self.nFeat + self.increase * depth_id,
+                                     bn=self.bn, relu=False),                                     # ######### Index: 0
+                       self.resBlock(self.nFeat + self.increase * depth_id, self.nFeat + self.increase * (depth_id + 1),
+                                                                                                  # ######### Index: 1
+                                     bn=self.bn),
+                       self.resBlock(self.nFeat + self.increase * (depth_id + 1), self.nFeat + self.increase * depth_id,
+                                                                                                   # ######### Index: 2
+                                     bn=self.bn),
+                       self.resBlock(self.nFeat + self.increase * depth_id, self.nFeat + self.increase * depth_id,
+                                                                                                   # ######### Index: 3
+                                     bn=self.bn),
+                       self.resBlock(self.nFeat + self.increase * depth_id, self.nFeat + self.increase * depth_id,
+                                                                                                   # ######### Index: 4
+                                     bn=self.bn, relu=False),
+                       nn.LeakyReLU(negative_slope=0.01, inplace=True)                              # ######### Index: 5
+                       ]
+        return pack_layers
 
-#     def _make_hour_glass(self):
-#         """
-#         pack conve layers modules of hourglass block
-#         :return: conve layers packed in n hourglass blocks
-#         """
-#         hg = []
-#         for i in range(self.depth):
-#             #  skip path; up_residual_block; down_residual_block_path,
-#             # 0 ~ n-2 (except the outermost n-1 order) need 3 residual blocks
-#             res = self._make_lower_residual(i)  # type:list
-#             if i == (self.depth - 1):  # the deepest path (i.e. the longest path) need 4 residual blocks
-#                 res.append(self._make_single_residual(i))  # list append an element
-#             hg.append(nn.ModuleList(res))  # pack conve layers of  every oder of hourglass block
-#         return nn.ModuleList(hg)
+    def _make_hour_glass(self):
+        """
+        pack conve layers modules of hourglass block
+        :return: conve layers packed in n hourglass blocks
+        """
+        hg = []
+        for i in range(self.depth):
+            #  skip path; up_residual_block; down_residual_block_path,
+            # 0 ~ n-2 (except the outermost n-1 order) need 3 residual blocks
+            res = self._make_lower_residual(i)  # type:list
+            if i == (self.depth - 1):  # the deepest path (i.e. the longest path) need 4 residual blocks
+                res.append(self._make_single_residual(i))  # list append an element
+            hg.append(nn.ModuleList(res))  # pack conve layers of  every oder of hourglass block
+        return nn.ModuleList(hg)
 
-#     def _hour_glass_forward(self, depth_id, x, up_fms):
-#         """
-#         built an hourglass block whose order is depth_id
-#         :param depth_id: oder number of hourglass block
-#         :param x: input tensor
-#         :return: output tensor through an hourglass block
-#         """
-#         up1 = self.hg[depth_id][0](x)
-#         low1 = self.downsample(x)
-#         low1 = self.hg[depth_id][1](low1)
-#         if depth_id == (self.depth - 1):  # except for the highest-order hourglass block
-#             low2 = self.hg[depth_id][6](low1)
-#         else:
-#             # call the lower-order hourglass block recursively
-#             low2 = self._hour_glass_forward(depth_id + 1, low1, up_fms)
-#         low3 = self.hg[depth_id][2](low2)
-#         up_fms.append(low2)
-#         # ######################## # if we don't consider 8*8 scale
-#         # if depth_id < self.depth - 1:
-#         #     self.up_fms.append(low2)
-#         up2 = self.upsample(low3)
-#         deconv1 = self.hg[depth_id][3](up2)
-#         deconv2 = self.hg[depth_id][4](deconv1)
-#         up1 += deconv2
-#         out = self.hg[depth_id][5](up1)  # relu after residual add
-#         return out
+    def _hour_glass_forward(self, depth_id, x, up_fms):
+        """
+        built an hourglass block whose order is depth_id
+        :param depth_id: oder number of hourglass block
+        :param x: input tensor
+        :return: output tensor through an hourglass block
+        """
+        up1 = self.hg[depth_id][0](x)
+        low1 = self.downsample(x)
+        low1 = self.hg[depth_id][1](low1)
+        if depth_id == (self.depth - 1):  # except for the highest-order hourglass block
+            low2 = self.hg[depth_id][6](low1)
+        else:
+            # call the lower-order hourglass block recursively
+            low2 = self._hour_glass_forward(depth_id + 1, low1, up_fms)
+        low3 = self.hg[depth_id][2](low2)
+        up_fms.append(low2)
+        # ######################## # if we don't consider 8*8 scale
+        # if depth_id < self.depth - 1:
+        #     self.up_fms.append(low2)
+        up2 = self.upsample(low3)
+        deconv1 = self.hg[depth_id][3](up2)
+        deconv2 = self.hg[depth_id][4](deconv1)
+        up1 += deconv2
+        out = self.hg[depth_id][5](up1)  # relu after residual add
+        return out
 
-#     def forward(self, x):
-#         """
-#         :param: x a input tensor warpped wrapped as a list
-#         :return: 5 different scales of feature maps, 128*128, 64*64, 32*32, 16*16, 8*8
-#         """
-#         up_fms = []  # collect feature maps produced by low2 at every scale
-#         feature_map = self._hour_glass_forward(0, x, up_fms)
-#         return [feature_map] + up_fms[::-1]
+    def forward(self, x):
+        """
+        :param: x a input tensor warpped wrapped as a list
+        :return: 5 different scales of feature maps, 128*128, 64*64, 32*32, 16*16, 8*8
+        """
+        up_fms = []  # collect feature maps produced by low2 at every scale
+        feature_map = self._hour_glass_forward(0, x, up_fms)
+        return [feature_map] + up_fms[::-1]
 
 
 
